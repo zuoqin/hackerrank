29ce8e2f417e3e2b458e0143048cc9074351bdd5
Alexey Zorchenkov <alexey@t5p.root>
2016-12-14 17:47:15 +0800

    Merge List https://www.hackerrank.com/challenges/merge-list solved

--- src/MergeList.hs
+++ src/MergeList.hs
@@ -2,5 +2,30 @@ module MergeList
     ( mergelist
     ) where
 
+choose :: Integer -> Integer -> Integer
+choose n 0 = 1
+choose 0 k = 0
+choose n k = ( choose (n-1) (k-1) * n `div` k )
+
+
 mergelist :: IO ()
-mergelist = putStrLn "mergelist"
+mergelist = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    getMultipleLines n
+
+getMultipleLines :: Int -> IO ()
+getMultipleLines cnt
+    | cnt <= 0 = putStrLn ""
+    | otherwise = do
+        
+        x_temp <- getLine
+        let x_t = words x_temp
+
+        let m = read $ x_t!!0 :: Integer
+        let n = read $ x_t!!1 :: Integer
+        
+        print ( (choose (n+m) m) `mod` 1000000007)
+        
+        getMultipleLines (cnt-1)
+
