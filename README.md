c32de6da266832e36cc236572b067f827c4f520a
Alexey Zorchenkov <alexey@t5p.root>
2016-12-20 08:59:13 +0800

    Is Fibo has been added

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 2. Merge List: https://www.hackerrank.com/challenges/merge-list
 3. Filling Jars: https://www.hackerrank.com/challenges/filling-jars
 4. Rectangular Game: https://www.hackerrank.com/challenges/rectangular-game
+5. Special Multiple: https://www.hackerrank.com/challenges/special-multiple
+6. Is Fibo: https://www.hackerrank.com/challenges/is-fibo
---
+++ src/IsFibo.hs
@@ -0,0 +1,32 @@
+module IsFibo
+    ( isFibo,
+      isFiboCases
+    ) where
+  
+import Numeric
+import Data.Char
+import Data.Set as Set
+
+fibonaci :: Num b => [b]
+fibonaci = Prelude.map fst (iterate f (0,1)) where f (x,y) = (y,x+y)
+
+isFiboCases :: Int -> Set Int  -> IO()
+isFiboCases n m
+    | n <= 0 = putStrLn ""
+    | otherwise = do          
+        n_temp <- getLine
+        let num = read n_temp :: Int
+
+        let val = Set.member num m
+
+        if val == True then putStrLn "IsFibo" else putStrLn "IsNotFibo"
+
+        isFiboCases (n-1) m
+        
+
+isFibo ::  IO ()
+isFibo = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    isFiboCases n (Set.fromList(takeWhile (<1000) fibonaci))
+    
